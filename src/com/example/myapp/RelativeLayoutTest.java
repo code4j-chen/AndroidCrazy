0545cd018b60aa2bf077eba07b245201827dd718
code4j-chen <code4j.chen@gmail.com>
2015-03-03 19:47:03 +0800

    相对布局-梅花效果

---
+++ src/com/example/myapp/RelativeLayoutTest.java
@@ -0,0 +1,22 @@
+package com.example.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by code4j on 2015/3/3.
+ */
+public class RelativeLayoutTest extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
