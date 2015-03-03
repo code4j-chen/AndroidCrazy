ec11c7e7106bbc1ccdbccab28a585f23f3cefdf1
code4j-chen <code4j.chen@gmail.com>
2015-03-03 17:45:48 +0800

    使用xml中创建一个自定义绘制圆的视图

--- src/com/example/myapp/CustomView.java
+++ src/com/example/myapp/CustomView.java
@@ -2,7 +2,6 @@ package com.example.myapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.LinearLayout;
 
 /**
  * Created by code4j on 2015/3/3.
@@ -12,10 +11,5 @@ public class CustomView extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        LinearLayout layout = (LinearLayout) findViewById(R.id.root);
-        final DrawView draw = new DrawView(this);
-        draw.setMinimumHeight(500);
-        draw.setMinimumWidth(300);
-        layout.addView(draw);
     }
 }
