c7fd08e85ab47e1da2c7a98cbbc065bd2896ecab
code4j-chen <code4j.chen@gmail.com>
2015-03-09 11:09:38 +0800

    NinePatchTest

---
+++ src/com/example/myapp/NinePatchTest.java
@@ -0,0 +1,15 @@
+package com.example.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by code4j on 2015/3/9.
+ */
+public class NinePatchTest extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
