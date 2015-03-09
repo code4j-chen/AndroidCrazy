92fdb057666c22a40efaad16059c36fcf0b1acd8
code4j-chen <code4j.chen@gmail.com>
2015-03-09 12:09:19 +0800

    ClockTest

---
+++ src/com/example/myapp/ClockTest.java
@@ -0,0 +1,15 @@
+package com.example.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by code4j on 2015/3/6.
+ */
+public class ClockTest extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
