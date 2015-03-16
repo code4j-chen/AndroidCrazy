0c5dd6a713870fc15ab1630e47e2313e3eec6179
code4j-chen <code4j.chen@gmail.com>
2015-03-16 11:52:33 +0800

    ListViewTest

---
+++ src/com/example/myapp/ListViewTest.java
@@ -0,0 +1,15 @@
+package com.example.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by code4j on 2015/3/13.
+ */
+public class ListViewTest extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
