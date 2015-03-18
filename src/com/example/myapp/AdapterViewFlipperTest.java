ece924d95489c79cba5b4995a2e6a1de33b5d6fe
code4j-chen <code4j.chen@gmail.com>
2015-03-18 11:36:14 +0800

    AdapterViewFlipperTest

--- src/com/example/myapp/AdapterViewFlipperTest.java
+++ src/com/example/myapp/AdapterViewFlipperTest.java
@@ -1,8 +1,9 @@
 package com.example.myapp;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.res.TypedArray;
-import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,7 @@ import android.widget.*;
 /**
  * Created by code4j on 2015/3/6.
  */
-public class GalleryTest extends Activity {
+public class AdapterViewFlipperTest extends Activity {
     int[] imageIds = new int[]
             {
                     R.drawable.shuangzi, R.drawable.shuangyu,
@@ -19,15 +20,14 @@ public class GalleryTest extends Activity {
                     R.drawable.sheshou, R.drawable.juxie, R.drawable.shuiping,
                     R.drawable.shizi, R.drawable.baiyang, R.drawable.jinniu,
                     R.drawable.mojie };
-    Gallery gallery;
-    ImageView imageView;
+    AdapterViewFlipper flipper;
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        gallery = (Gallery) findViewById(R.id.gallery);
-
-        imageView = (ImageView) findViewById(R.id.imageView);
+        flipper = (AdapterViewFlipper) findViewById(R.id.flipper);
+        // 创建一个BaseAdapter对象，该对象负责提供Gallery所显示的列表项
         BaseAdapter adapter = new BaseAdapter() {
             @Override
             public int getCount() {
@@ -46,27 +46,39 @@ public class GalleryTest extends Activity {
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
-                ImageView imageView = new ImageView(GalleryTest.this);
+                ImageView imageView = new ImageView(AdapterViewFlipperTest.this);
                 imageView.setImageResource(imageIds[position]);
                 imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-                imageView.setLayoutParams(new Gallery.LayoutParams(75, 100));
-                TypedArray typedArray = obtainStyledAttributes(R.styleable.Gallery);
-                imageView.setBackgroundResource(typedArray.getResourceId(
-                        R.styleable.Gallery_android_galleryItemBackground, 0));
+                // 为imageView设置布局参数
+                imageView.setLayoutParams(new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
                 return imageView;
             }
         };
-        gallery.setAdapter(adapter);
-        gallery.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                imageView.setImageResource(imageIds[position]);
-            }
+        flipper.setAdapter(adapter);
+    }
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public void prev(View source)
+    {
+        // 显示上一个组件
+        flipper.showPrevious();
+        // 停止自动播放
+        flipper.stopFlipping();
+    }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public void next(View source)
+    {
+        // 显示下一个组件。
+        flipper.showNext();
+        // 停止自动播放
+        flipper.stopFlipping();
+    }
 
-            }
-        });
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public void auto(View source)
+    {
+        // 开始自动播放
+        flipper.startFlipping();
     }
 }
