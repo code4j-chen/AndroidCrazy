163436fbd3a4524aed317ea1510a18710cd6cf79
code4j-chen <code4j.chen@gmail.com>
2015-03-25 16:43:59 +0800

    ActionBarTest

---
+++ ActionBarTest/src/com/example/ActionBarTest/ActionBarTest.java
@@ -0,0 +1,28 @@
+package com.example.ActionBarTest;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActionBarTest extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+
+    ActionBar actionBar;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        actionBar = getActionBar();
+    }
+
+    public void showActionBar(View view) {
+        actionBar.show();
+    }
+
+    public void hideActionBar(View view) {
+        actionBar.hide();
+    }
+}
