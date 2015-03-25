9b075247a919649c30acedf5e0ab0f74b6890108
code4j-chen <code4j.chen@gmail.com>
2015-03-25 18:18:58 +0800

    ActionViewTest

---
+++ ActionViewTest/src/com/example/ActionViewTest/ActionViewTest.java
@@ -0,0 +1,23 @@
+package com.example.ActionViewTest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class ActionViewTest extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
