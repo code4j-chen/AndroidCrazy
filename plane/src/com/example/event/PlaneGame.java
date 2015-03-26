4ca1140eb507ecec2a056d5817df359da7da595c
code4j-chen <code4j.chen@gmail.com>
2015-03-26 12:08:22 +0800

    plane

---
+++ plane/src/com/example/event/PlaneGame.java
@@ -0,0 +1,57 @@
+package com.example.event;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.*;
+
+public class PlaneGame extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    // 定义飞机的移动速度
+    private int speed = 10;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        final PlaneView planeView = new PlaneView(this);
+        setContentView(planeView);
+        planeView.setBackgroundResource(R.drawable.back);
+        WindowManager windowManager = getWindowManager();
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        planeView.currentX = metrics.widthPixels /2;
+        planeView.currentY = metrics.heightPixels -40;
+        planeView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // 获取由哪个键触发的事件
+                switch (event.getKeyCode())
+                {
+                    // 控制飞机下移
+                    case KeyEvent.KEYCODE_S:
+                        planeView.currentY += speed;
+                        break;
+                    // 控制飞机上移
+                    case KeyEvent.KEYCODE_W:
+                        planeView.currentY -= speed;
+                        break;
+                    // 控制飞机左移
+                    case KeyEvent.KEYCODE_A:
+                        planeView.currentX -= speed;
+                        break;
+                    // 控制飞机右移
+                    case KeyEvent.KEYCODE_D:
+                        planeView.currentX += speed;
+                        break;
+                }
+                // 通知planeView组件重绘
+                planeView.invalidate();
+                return true;
+            }
+        });
+    }
+}
---
+++ plane/src/com/example/event/PlaneView.java
@@ -0,0 +1,29 @@
+package com.example.event;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+/**
+ * Created by code4j on 2015/3/26.
+ */
+public class PlaneView extends View {
+    public float currentX;
+    public float currentY;
+    Bitmap plane;
+    public PlaneView(Context context) {
+        super(context);
+        plane = BitmapFactory.decodeResource(context.getResources(), R.drawable.plane);
+        setFocusable(true);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        canvas.drawBitmap(plane, currentX, currentX, paint);
+    }
+}
