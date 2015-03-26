6a5531d259dead3891f3bdc58503032b57429017
code4j-chen <code4j.chen@gmail.com>
2015-03-26 15:44:37 +0800

    ActivityListener

---
+++ ActivityListener/src/com/example/ActivityListener/ActivityListener.java
@@ -0,0 +1,31 @@
+package com.example.ActivityListener;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ActivityListener extends Activity implements View.OnClickListener{
+    /**
+     * Called when the activity is first created.
+     */
+
+    EditText show;
+    Button bn;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        show = (EditText) findViewById(R.id.show);
+        bn = (Button) findViewById(R.id.bn);
+        // 直接使用Activity作为事件监听器
+        bn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        show.setText("bn按钮被单击了！");
+    }
+}
