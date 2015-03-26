3605fa2ee808ad1bd8c7123423cc1b045d974315
code4j-chen <code4j.chen@gmail.com>
2015-03-26 11:13:48 +0800

    ActionBar_TabSwipeNav

---
+++ ActionBar_TabSwipeNav/src/com/example/ActionBar_TabSwipeNav/DummyFragment.java
@@ -0,0 +1,38 @@
+package com.example.ActionBar_TabSwipeNav;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Description:
+ * <br/>网站: <a href="http://www.crazyit.org">疯狂Java联盟</a>
+ * <br/>Copyright (C), 2001-2014, Yeeku.H.Lee
+ * <br/>This program is protected by copyright laws.
+ * <br/>Program Name:
+ * <br/>Date:
+ * @author Yeeku.H.Lee kongyeeku@163.com
+ * @version 1.0
+ */
+public class DummyFragment extends Fragment 
+{
+	public static final String ARG_SECTION_NUMBER = "section_number";
+	// 该方法的返回值就是该Fragment显示的View组件
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState)
+	{
+		TextView textView = new TextView(getActivity());
+		textView.setGravity(Gravity.CENTER_HORIZONTAL);
+		// 获取创建该Fragment时传入的参数Bundle
+		Bundle args = getArguments();
+		// 设置TextView显示的文�?		textView.setText(args.getInt(ARG_SECTION_NUMBER) + "");
+		textView.setTextSize(30);
+		// 返回该TextView
+		return textView;
+	}
+}
